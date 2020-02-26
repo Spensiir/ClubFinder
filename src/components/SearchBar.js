a031c13c2f560ce1c0375dc19e53e47588df1232
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2020-02-26 17:58:09 -0500

    Revert "Update SearchBar.js"
    This reverts commit a39643ba301339a76dea59e67576ed0df20fa747.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,6 +10,9 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
                 <input type="text" placeholder="Search.."></input>
             </div>
         )
