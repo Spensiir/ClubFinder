a39643ba301339a76dea59e67576ed0df20fa747
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2020-02-26 17:55:33 -0500

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,9 +10,6 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
                 <input type="text" placeholder="Search.."></input>
             </div>
         )
