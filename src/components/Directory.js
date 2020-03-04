510f48eefc1ba1105670f92ea62001078ab6d6f0
Sterling Velazquez <svelazquez6@gatech.edu>
2020-03-04 13:51:21 -0500

    Update Directory.js

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -18,11 +18,11 @@ class Directory extends React.Component {
                 <button onClick={activeBtn()} className="btn1 active">Clubs</button>
                 <button onClick={activeBtn()} className="btn1">Organizations</button>
                 <br/>
-                <input onKeyUp={searchFunction()} id="searchInput" type="text" placeholder="Search.." name="search"></input>
+                <input id="searchInput" type="text" placeholder="Search.." name="search"></input>
                 <button className="btn2" type="submit"><i className="fa fa-search"></i></button>
                 <br/>
                 <ul>
-                { //Add a list of Markers to Your Map
+                { 
                     randomArr.map( (each) =>
                         <li key={keyVal++} id="listItem">{each}</li>
                     )
@@ -43,23 +43,5 @@ function activeBtn() {
         this.className += " active";
     });
 }}
-
-function searchFunction() {
-    // Declare variables
-    var input, li, a, i, txtValue;
-    input = document.getElementById("searchInput");
-    console.log(input);
-    li = document.getElementsByTagName("li");
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
 
 export default Directory;
