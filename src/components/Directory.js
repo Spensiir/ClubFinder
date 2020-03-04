30ffd12d8fc749f328f8b20aac1e6ed367378072
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2020-03-04 12:40:09 -0500

    search function attempt (not working)

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -2,6 +2,7 @@ import React from "react"
 import "../css/directory.css"
 
 const randomArr = ["potato", "tomato", "eggplant", "flamin' hot cheetos", "zucchini"];
+var keyVal = 0;
 
 class Directory extends React.Component {
 
@@ -17,13 +18,13 @@ class Directory extends React.Component {
                 <button onClick={activeBtn()} className="btn1 active">Clubs</button>
                 <button onClick={activeBtn()} className="btn1">Organizations</button>
                 <br/>
-                <input type="text" placeholder="Search.." name="search"></input>
+                <input onKeyUp={searchFunction()} id="searchInput" type="text" placeholder="Search.." name="search"></input>
                 <button className="btn2" type="submit"><i className="fa fa-search"></i></button>
                 <br/>
                 <ul>
                 { //Add a list of Markers to Your Map
                     randomArr.map( (each) =>
-                        <li>{each}</li>
+                        <li key={keyVal++} id="listItem">{each}</li>
                     )
                 }
                 </ul>
@@ -42,5 +43,23 @@ function activeBtn() {
         this.className += " active";
     });
 }}
+
+function searchFunction() {
+    // Declare variables
+    var input, li, a, i, txtValue;
+    input = document.getElementById("searchInput");
+    console.log(input);
+    li = document.getElementsByTagName("li");
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
 
 export default Directory;
